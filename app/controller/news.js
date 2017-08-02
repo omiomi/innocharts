adf57594a0c988b2f5d2867af552639f916736a5
wy88y <1953421292@qq.com>
2017-08-02 20:15:50 +0800

    WebStorm 提交

--- app/controller/news.js
+++ app/controller/news.js
@@ -8,7 +8,7 @@ module.exports = app => {
 			const dataList = {
 				list: [
 					{ id: 1, title: 'this is news 1', url: '/news/1' },
-					{ id: 2, title: 'this is news 2', url: '/news/2' }
+					{ id: 2, title: 'this is news 2', url: '/news/2' },
 				]
 			};
 			yield this.ctx.render('news/list.tpl', dataList);
